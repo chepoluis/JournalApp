36c5480bacc1ef371e05418e91aa54fefd3fac0c
Jose  luis Villa <vsjoseluisvs@gmail.com>
2022-07-05 20:34:53 -0500

    UI

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,47 @@
+import { TurnedInNot } from "@mui/icons-material"
+import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
+
+export const SideBar = ({ drawerWidth = 240 }) => {
+    return (
+        <Box
+            component='nav'
+            sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        >
+            <Drawer
+                variant="permanent" // Temporary
+                open
+                sx={{
+                    display: { xs: 'block' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+                }}
+            >
+                <Toolbar>
+                    <Typography variant="h6" noWrap component='div'>
+                        Luis
+                    </Typography>
+                </Toolbar>
+
+                <Divider />
+
+                <List>
+                    {
+                        ['Hola', 'Hola2', 'Hola3', 'Hola4'].map( text => (
+                            <ListItem key={ text } disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <TurnedInNot />
+                                    </ListItemIcon>
+
+                                    <Grid container>
+                                        <ListItemText primary={ text }/>
+                                        <ListItemText secondary={ 'lorem lorem lorem hola' } />
+                                    </Grid>
+                                </ListItemButton>
+                            </ListItem>
+                        ))
+                    }
+                </List>
+            </Drawer>
+        </Box>
+    )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,8 +1,33 @@
-import { Typography } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView, NoteView } from "../views"
 
 export const JournalPage = () => {
     return (
-        // <Typography component='h1'>JournalPage</Typography> // No cambia como tal la etiqueta h1
-        <Typography variant='h1'>JournalPage</Typography>
+        <JournalLayout>
+            {/* // <Typography component='h1'>JournalPage</Typography> // No cambia como tal la etiqueta h1 */}
+            {/* <Typography variant='h1'>JournalPage</Typography> */}
+            {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, necessitatibus voluptas fuga, dolorum commodi officia similique animi est repellat consequatur recusandae quia magnam? Alias accusantium nihil corrupti nisi blanditiis? Beatae!</Typography> */}
+            
+
+            <NothingSelectedView />
+
+            <IconButton
+                size='large'
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }} />
+            </IconButton>
+
+            {/* <NoteView /> */}
+        </JournalLayout>
     )
 }
