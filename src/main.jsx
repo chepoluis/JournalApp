6e8fdfd3956a0eaecc6a3078008dc92587fb187a
Jose  luis Villa <vsjoseluisvs@gmail.com>
2022-08-08 22:00:26 -0500

    redux configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import { JournalApp } from './JournalApp';
+import { store } from './store';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // Una de las caracteristicas de StrictMode es que dispara el useEffect useLayoutEffect dos veces
-  <React.StrictMode> 
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+  <React.StrictMode>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
