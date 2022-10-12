8621e9c96cfe287b19a805a9af7fde9ff2e3c22a
Jose  luis Villa <vsjoseluisvs@gmail.com>
2022-10-11 20:06:33 -0500

    Form validations

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,8 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 
-export const useForm = ( initialForm = {} ) => {
+export const useForm = ( initialForm = {}, formValidations = {} ) => {
   
     const [ formState, setFormState ] = useState( initialForm );
+    const [formValidation, setFormValidation] = useState({})
+
+    useEffect(() => {
+        createValidators();
+
+    }, [formState]);
+    
+    const isFormValid = useMemo( () => {
+        
+        for (const formKey of Object.keys(formValidation)) {
+            // Null = no errors
+            if (formValidation[formKey] !== null)
+                return false;
+        }
+
+        return true;
+    }, [formValidation]);
 
     const onInputChange = ({ target }) => {
         const { name, value } = target;
@@ -16,10 +33,31 @@ export const useForm = ( initialForm = {} ) => {
         setFormState( initialForm );
     }
 
+    const createValidators = () => {
+        const formCheckedValues = {};
+
+        // Imprimir el nombre de las propiedades
+        for (const formField of Object.keys(formValidations)) {
+            // Destructuramos el array, obteniendo la funcion y el error message
+            const [ fn, errorMessage ] = formValidations[formField];
+
+            /**
+             * Se crea una nueva propiedad si el texto no cumple con la condicion
+             * formState[formField] -> se accede al valor del field y se pasa a la funcion para validar el valor
+             */
+            formCheckedValues[`${ formField }Valid`] = fn( formState[formField] ) ? null : errorMessage;
+
+            // console.log(formCheckedValues);
+            setFormValidation( formCheckedValues );
+        }
+    }
+
     return {
         ...formState,
+        ...formValidation,
         formState,
         onInputChange,
         onResetForm,
+        isFormValid
     }
 }
